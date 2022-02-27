c19e3cb9e53932421866a26205642b4c46a1eead
Jeffrey Edison <jeffrey.edison@gmail.com>
2022-02-27 14:58:48 +0100

    Update highlight-custom.js

--- assets/highlight-custom.js
+++ assets/highlight-custom.js
@@ -1,5 +1,5 @@
 //Ref: https://highlightjs.readthedocs.io/en/latest/index.html
 //Initialise highlight js on <pre></code> blocks
 // hljs.initHighlighting();
-alert(hljs.getLanguage("json"));
+console.log("calling hljs");
 hljs.highlightAll();
