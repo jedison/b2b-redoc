40e12d0249edaa898f902ef63f9845297a777b8d
Jeffrey Edison <jeffrey.edison@gmail.com>
2022-02-27 15:36:08 +0100

    highlighting
    Add highlighting after load of postman collection section

--- assets/highlight-custom.js
+++ assets/highlight-custom.js
@@ -1,5 +1,8 @@
 //Ref: https://highlightjs.readthedocs.io/en/latest/index.html
 //Initialise highlight js on <pre></code> blocks
 // hljs.initHighlighting();
-console.log("calling hljs");
-hljs.highlightAll();
+function doHighlighting() {
+    console.log("calling hljs");
+    hljs.highlightAll();
+}
+doHighlighting();
