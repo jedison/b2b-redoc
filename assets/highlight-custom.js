00276ac1bb3ea546b83f737c167ae0b0f44f7615
Jeffrey Edison <jeffrey.edison@gmail.com>
2022-02-27 15:41:11 +0100

    higlighting
    highlighting postman collection within section

--- assets/highlight-custom.js
+++ assets/highlight-custom.js
@@ -1,8 +1,5 @@
 //Ref: https://highlightjs.readthedocs.io/en/latest/index.html
 //Initialise highlight js on <pre></code> blocks
 // hljs.initHighlighting();
-function doHighlighting() {
-    console.log("calling hljs");
-    hljs.highlightAll();
-}
-doHighlighting();
+// console.log("calling hljs");
+hljs.highlightAll();
