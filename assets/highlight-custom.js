b55591de9aa41e05054b1e72e07fbe20e7202e35
Jeffrey Edison <jeffrey.edison@gmail.com>
2022-02-27 14:42:04 +0100

    update highlight and add template defined in redocly

--- assets/highlight-custom.js
+++ assets/highlight-custom.js
@@ -1,3 +1,4 @@
 //Ref: https://highlightjs.readthedocs.io/en/latest/index.html
 //Initialise highlight js on <pre></code> blocks
-hljs.initHighlighting();
+// hljs.initHighlighting();
+hljs.highlightAll();
