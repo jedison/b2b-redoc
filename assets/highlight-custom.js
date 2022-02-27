a0c15ef223d78e4665ef927dde18c439b69afd19
Jeffrey Edison <jeffrey.edison@gmail.com>
2022-02-27 15:45:40 +0100

    higlighting
    Try to make highlight work onload

--- assets/highlight-custom.js
+++ assets/highlight-custom.js
@@ -1,5 +1,8 @@
 //Ref: https://highlightjs.readthedocs.io/en/latest/index.html
 //Initialise highlight js on <pre></code> blocks
 // hljs.initHighlighting();
-// console.log("calling hljs");
-hljs.highlightAll();
+function doHighlight() {
+    console.log("calling hljs");
+    hljs.highlightAll();
+}
+doHighlight();
