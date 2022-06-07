9b1af5d372aac19bd7466819d550659245b7a48e
Tarik Handanovic <tarik.handanovic@crscreditapi.com>
2022-06-07 11:48:53 +0200

    Added automatic authorization

---
+++ assets/js/set-token.js
@@ -0,0 +1,15 @@
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var token = getParameterByName('token');
+
+if (token) {
+    console.log(token);
+    sessionStorage.setItem("redoc.auth.Authorization Token", token)
+}
